6267af054ff468add6e972c636e9c41df9930ac3
3lswear <r.malyavsky@protonmail.com>
2021-01-06 05:42:57 +0300

    init

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/06 02:55:03 by sunderle          #+#    #+#             */
+/*   Updated: 2021/01/06 05:41:25 by sunderle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int get_next_line(int fd, char **line);
+
+#endif
