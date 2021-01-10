21cb84549a9421c9c91c0591e929b96c8496b5af
3lswear <r.malyavsky@protonmail.com>
2021-01-10 04:46:07 +0300

    kinda working with buffer < line, one line only

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 05:37:12 by sunderle          #+#    #+#             */
-/*   Updated: 2021/01/10 04:02:47 by sunderle         ###   ########.fr       */
+/*   Updated: 2021/01/10 04:45:16 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -14,26 +14,36 @@
 
 int	get_next_line(int fd, char **line)
 {
-	static size_t pos;
+	size_t pos;
+	static size_t linebreak;
 	char *buf;
 	ssize_t ret;
 
 	buf = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 
-	pos = 0;
 	*line = (char *)ft_calloc(sizeof(char), BUFFER_SIZE + 1);
+	pos = linebreak;
 	while ((ret = read(fd, buf, BUFFER_SIZE)))
 	{
 		printf("the buffer is =>[%s]\n", buf);
 		while (buf[pos % BUFFER_SIZE] != '\n')
 		{
-			*line = ft_reallocarray(*line, sizeof(char), pos + 1);
+			*line = ft_reallocarray(*line, sizeof(char), pos + 1 - linebreak);
 			pos++;
+			if ((pos % BUFFER_SIZE) == 0)
+				break;
 		}
 		if (buf[pos % BUFFER_SIZE] == '\n')
+		{
+			ft_memcpy(&(*line)[ft_strlen(*line)], buf, pos % BUFFER_SIZE);
+			linebreak = pos;
 			break;
+		}
+		else
+		{
+			ft_memcpy(*line, buf, BUFFER_SIZE);
+		}
 	}
-	ft_memcpy(*line, buf, pos % BUFFER_SIZE);
 
 	/* *line = (char *)ft_calloc(sizeof(char), pos + 1); */
 
