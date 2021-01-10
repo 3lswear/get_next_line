19c0cd9990ad94d1745b3a965129bba732ab93e3
3lswear <r.malyavsky@protonmail.com>
2021-01-10 04:04:37 +0300

    working for lines < buf

--- get_next_line.c
+++ get_next_line.c
@@ -6,35 +6,44 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 05:37:12 by sunderle          #+#    #+#             */
-/*   Updated: 2021/01/07 05:08:15 by sunderle         ###   ########.fr       */
+/*   Updated: 2021/01/10 04:02:47 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include <stdio.h>
 #include "get_next_line.h"
 
 int	get_next_line(int fd, char **line)
 {
 	static size_t pos;
-	size_t read_pos;
-	char c;
 	char *buf;
 	ssize_t ret;
 
-	buf = (char *)malloc(BUFFER_SIZE);
+	buf = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 
 	pos = 0;
-	while ((ret = read(fd, buf, 1)) && (buf[0] != '\n'))
-	{
-		pos++;
-	}
-	line = (char **)malloc(sizeof(char *));
-	line[0] = (char *)ft_calloc(sizeof(char), pos + 1);
-	read_pos = 0;
+	*line = (char *)ft_calloc(sizeof(char), BUFFER_SIZE + 1);
 	while ((ret = read(fd, buf, BUFFER_SIZE)))
 	{
-		ft_memcpy(&(line[0][read_pos]), buf, ret);
-		read_pos += ret;
+		printf("the buffer is =>[%s]\n", buf);
+		while (buf[pos % BUFFER_SIZE] != '\n')
+		{
+			*line = ft_reallocarray(*line, sizeof(char), pos + 1);
+			pos++;
+		}
+		if (buf[pos % BUFFER_SIZE] == '\n')
+			break;
 	}
+	ft_memcpy(*line, buf, pos % BUFFER_SIZE);
+
+	/* *line = (char *)ft_calloc(sizeof(char), pos + 1); */
+
+	/* read_pos = 0; */
+	/* while ((read_pos < pos) && (ret = read(fd, buf, BUFFER_SIZE))) */
+	/* { */
+	/* 	printf("read a buffer\n"); */
+	/* 	ft_memcpy(&((*line)[read_pos]), buf, ret); */
+	/* 	read_pos += ret; */
+	/* } */
 
 	return (1);
 }
