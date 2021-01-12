5db67f5ecc853e806ca626a726c7320265739d10
3lswear <r.malyavsky@protonmail.com>
2021-01-12 03:29:07 +0300

    now getting new buffer on demand

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 05:37:12 by sunderle          #+#    #+#             */
-/*   Updated: 2021/01/10 04:45:16 by sunderle         ###   ########.fr       */
+/*   Updated: 2021/01/12 03:26:00 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -14,18 +14,31 @@
 
 int	get_next_line(int fd, char **line)
 {
-	size_t pos;
-	static size_t linebreak;
-	char *buf;
+	size_t pos; //position where we are seeking in fd
+	static size_t linebreak; //previous line break position
+	char buf[BUFFER_SIZE + 1];
 	ssize_t ret;
 
-	buf = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	/* buf = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char)); */
 
 	*line = (char *)ft_calloc(sizeof(char), BUFFER_SIZE + 1);
-	pos = linebreak;
-	while ((ret = read(fd, buf, BUFFER_SIZE)))
+	if (!linebreak)
 	{
-		printf("the buffer is =>[%s]\n", buf);
+		ft_bzero(buf, BUFFER_SIZE);
+		pos = linebreak;
+	}
+	else
+		pos = linebreak + 1;
+	while (1)
+	{
+		if ((pos % BUFFER_SIZE) == 0)
+		//if reached end of buffer, get a new one
+		{
+			if (!(ret = read(fd, buf, BUFFER_SIZE)))
+				break;
+			/* pos++; */
+		}
+		/* printf("the buffer is =>[%s]\n", buf); */
 		while (buf[pos % BUFFER_SIZE] != '\n')
 		{
 			*line = ft_reallocarray(*line, sizeof(char), pos + 1 - linebreak);
