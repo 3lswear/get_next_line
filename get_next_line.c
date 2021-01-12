9f2734fff4cc675722b3251183a5e5c120f33c17
3lswear <r.malyavsky@protonmail.com>
2021-01-12 18:08:52 +0300

    changed linebreak meaning, fixed short lines behaviour

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 05:37:12 by sunderle          #+#    #+#             */
-/*   Updated: 2021/01/12 03:26:00 by sunderle         ###   ########.fr       */
+/*   Updated: 2021/01/12 18:04:07 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -15,7 +15,7 @@
 int	get_next_line(int fd, char **line)
 {
 	size_t pos; //position where we are seeking in fd
-	static size_t linebreak; //previous line break position
+	static size_t linebreak; //previous line break position + 1
 	char buf[BUFFER_SIZE + 1];
 	ssize_t ret;
 
@@ -28,7 +28,7 @@ int	get_next_line(int fd, char **line)
 		pos = linebreak;
 	}
 	else
-		pos = linebreak + 1;
+		pos = linebreak;
 	while (1)
 	{
 		if ((pos % BUFFER_SIZE) == 0)
@@ -36,9 +36,8 @@ int	get_next_line(int fd, char **line)
 		{
 			if (!(ret = read(fd, buf, BUFFER_SIZE)))
 				break;
-			/* pos++; */
 		}
-		/* printf("the buffer is =>[%s]\n", buf); */
+		printf("\033[0;31m%2lu:[%s]\n\033[0m", pos / BUFFER_SIZE, buf);
 		while (buf[pos % BUFFER_SIZE] != '\n')
 		{
 			*line = ft_reallocarray(*line, sizeof(char), pos + 1 - linebreak);
@@ -46,17 +45,19 @@ int	get_next_line(int fd, char **line)
 			if ((pos % BUFFER_SIZE) == 0)
 				break;
 		}
-		if (buf[pos % BUFFER_SIZE] == '\n') //found newline
+		if ((pos % BUFFER_SIZE) && (buf[pos % BUFFER_SIZE] == '\n')) //found newline
 		{
-			if ((pos / BUFFER_SIZE) - 1 == (linebreak / BUFFER_SIZE)) //last break in this buffer
-				ft_memcpy(&(*line)[ft_strlen(*line)], &(buf[linebreak % BUFFER_SIZE]), pos % BUFFER_SIZE);
+			if (((pos - 1) / BUFFER_SIZE) == (linebreak / BUFFER_SIZE)) //check if last newline in this buffer
+				ft_memcpy(&(*line)[ft_strlen(*line)],
+						&(buf[(linebreak) % BUFFER_SIZE]),
+						(pos - linebreak));
 			else
 				ft_memcpy(&(*line)[ft_strlen(*line)], buf, pos % BUFFER_SIZE);
-			linebreak = pos;
+			linebreak = pos + 1;
 			break;
 		}
 		else //reached end of the buffer, no newline
-			if ((pos / BUFFER_SIZE) - 1 == (linebreak / BUFFER_SIZE)) //last break in this buffer
+			if (((pos - 1) / BUFFER_SIZE) == (linebreak / BUFFER_SIZE)) //check if last newline in this buffer
 				ft_memcpy(&(*line)[ft_strlen(*line)],
 						&(buf[(linebreak) % BUFFER_SIZE]),
 						BUFFER_SIZE - ((linebreak) % BUFFER_SIZE));
@@ -64,16 +65,6 @@ int	get_next_line(int fd, char **line)
 				ft_memcpy(&(*line)[ft_strlen(*line)], buf, BUFFER_SIZE);
 
 	}
-
-	/* *line = (char *)ft_calloc(sizeof(char), pos + 1); */
-
-	/* read_pos = 0; */
-	/* while ((read_pos < pos) && (ret = read(fd, buf, BUFFER_SIZE))) */
-	/* { */
-	/* 	printf("read a buffer\n"); */
-	/* 	ft_memcpy(&((*line)[read_pos]), buf, ret); */
-	/* 	read_pos += ret; */
-	/* } */
 
 	return (1);
 }
