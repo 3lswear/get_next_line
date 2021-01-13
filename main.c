62cd1113ae64180f2c9a5474d51f2e80b1e0e9de
3lswear <r.malyavsky@protonmail.com>
2021-01-13 14:03:35 +0300

    now calling in a loop

--- main.c
+++ main.c
@@ -46,23 +46,12 @@ int main()
 	char *line;
 	line = (char *)ft_calloc(sizeof(char), 1);
 
-	fd = open("./texts/game.txt", O_RDONLY);
-	/* fd = open("./texts/among.txt", O_RDONLY); */
-
-	get_next_line(fd, &line);
-	printf("the line is =>[%s]\n", line);
-
-	get_next_line(fd, &line);
-	printf("the line is =>[%s]\n", line);
-
-	get_next_line(fd, &line);
-	printf("the line is =>[%s]\n", line);
-
-	get_next_line(fd, &line);
-	printf("the line is =>[%s]\n", line);
-
-	get_next_line(fd, &line);
-	printf("the line is =>[%s]\n", line);
+	/* fd = open("./texts/game.txt", O_RDONLY); */
+	fd = open("./texts/among.txt", O_RDONLY);
+	while (get_next_line(fd, &line))
+	{
+		printf("the line is =>[%s]\n", line);
+	}
 
 	close(fd);
 
