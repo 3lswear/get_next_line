4169db72711733f75d2e9e77e28bba627915a5c4
3lswear <r.malyavsky@protonmail.com>
2021-01-20 03:36:10 +0300

    added BUFFER_SIZE output

--- main.c
+++ main.c
@@ -15,17 +15,19 @@ int main()
 ";
 
 	printf("%s", art);
+	printf("BUFFER_SIZE =>\t[%u]\n", BUFFER_SIZE);
 
 	int fd;
 	char *line;
 	line = (char *)ft_calloc(sizeof(char), 1);
 
 	/* fd = open("./texts/game.txt", O_RDONLY); */
-	fd = open("./texts/among.txt", O_RDONLY);
-	/* fd = 1; */
+	/* fd = open("./texts/among.txt", O_RDONLY); */
+	fd = 1;
 	while (get_next_line(fd, &line))
 	{
 		printf("the line is =>[%s]\n", line);
+		/* printf("%s\n", line); */
 		free(line);
 	}
 
