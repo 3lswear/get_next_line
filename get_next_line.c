47fe49d833abfce588259ac62b0d1d6be23dd0ca
3lswear <r.malyavsky@protonmail.com>
2021-01-22 00:06:24 +0300

    moved code to check_buf

--- get_next_line.c
+++ get_next_line.c
@@ -6,65 +6,79 @@
 /*   By: sunderle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 05:37:12 by sunderle          #+#    #+#             */
-/*   Updated: 2021/01/21 14:35:00 by sunderle         ###   ########.fr       */
+/*   Updated: 2021/01/22 00:03:36 by sunderle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 #include "get_next_line.h"
 
-ssize_t	check(char *substring, char **line)
+ssize_t	check_buf(char *substring, char **line, ssize_t ret)
 {
 	char *point;
+	char *tmp;
 
-	if (substring)
+	if ((point = ft_strchr_bd(substring, '\n', 0))) //found a newline
 	{
-		if ((point = ft_strchr_bd(substring, '\n', 0))) //found a newline
+		*point++ = 0;
+		if (!ret)
 		{
-			*point++ = 0;
 			if (!(*line = ft_strdup(substring)))
 				return (-1);
 			ft_strlcpy(substring, point, ft_strlen(point) + 1);
-			return (1); //copied a line to *line
 		}
-		else //no newline in substring
+		else
 		{
-			if (!(*line = ft_strdup(substring)))
+			tmp = *line;
+			if (!(*line = ft_strjoin(*line, substring)))
 				return (-1);
-			ft_bzero(substring, ft_strlen(substring) + 1);
+			free(tmp);
 		}
+		return (1); //copied a line to *line
 	}
-	else
-		*line = ft_calloc(sizeof(char), 1);
-	return (0); // no newline in substring, or no substring at all
+	else //no newline in substring
+	{
+		if (!ret)
+		{
+			*line = ft_strdup(substring);
+		}
+		else
+		{
+			tmp = *line;
+			*line = ft_strjoin(*line, substring);
+			free(tmp);
+		}
+		ft_bzero(substring, ft_strlen(substring) + 1);
+		if (!(*line))
+			return (-1);
+		return (0); // no newline in substring, or no substring at all
+	}
 }
 
 int		get_next_line(int fd, char **line)
 {
 	static char	substring[BUFFER_SIZE + 1];
 	ssize_t		ret;
-	ssize_t		rp;
-	char		*point;
-	char		*tmp;
+	ssize_t		res;
+	/* char		*point; */
+	/* char		*tmp; */
 
-	if (!line || (rp = check(&(substring[ft_strlen(substring) + 1]), line))
-			== -1)
+	ret = 0;
+	if (!line || (!BUFFER_SIZE) || (res = check_buf(&(substring
+		[ft_strlen(substring) + 1]), line, ret)) == -1 || fd < 0)
 		return (-1);
-	while (rp == 0 && (ret = read(fd, substring, BUFFER_SIZE)))
+	while (res == 0 && (ret = read(fd, substring, BUFFER_SIZE)) > 0)
 	{
-		/* printf("\033[0;31m[%s]:%lu\n\033[0m", substring, ft_strlen(substring)); */
 		substring[ret] = 0;
-		if ((point = ft_strchr_bd(substring, '\n', 0)))
-		{
-			*point++ = 0;
-			rp = 1;
-		}
-		tmp = *line;
-		if (!(*line = ft_strjoin(*line, substring)))
+		/* printf("\033[0;31m[%s]:%lu\n\033[0m", substring, ft_strlen(substring)); */
+		if ((res = check_buf(substring, line, ret)) == -1)
 			return (-1);
-		free(tmp);
 	}
-	if (rp == 0 && ret == 0)
+	if (res == 0 && ret == 0)
+	{
 		return (0);
+	}
+	else if (ret == -1)
+		return (-1);
 	else
 		return (1);
 }
